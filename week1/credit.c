b01c5986d44da82508ca43920628ce7670045c54
Emma Junger <ejjunger@proton.me>
2025-09-10 19:57:21 -0400

    started task 4

---
+++ week1/credit.c
@@ -0,0 +1,11 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void) {
+  // int min = 13;
+  // int max = 16;
+
+  string cc = get_string("Please enter a card number: ");
+
+  printf("%s", cc);
+}
