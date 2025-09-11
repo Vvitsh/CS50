014bcc73e174c891a18bad25f5772d0d8e544846
Emma Junger <ejjunger@proton.me>
2025-09-11 12:30:34 -0400

    Finished the primary logic for task 4 just need to clean up and add prints

--- week1/credit.c
+++ week1/credit.c
@@ -14,8 +14,8 @@ int checkSpecialChars(const char *str) {
   return 0;
 }
 
-void printArrayDebug(int arr[], int len) {
-  printf("Interger array: ");
+void printArrayDebug(int arr[], int len, string label) {
+  printf("Interger array %s: ", label);
   for (int i = 0; i < len; i++) {
     printf("%d", arr[i]);
   }
@@ -39,16 +39,19 @@ void reverseArray(int arr[], int len) {
 
 int main() {
   string cc_str;
-  int min = 13;
-  int max = 16;
+  // BUG: was getting warnings about the min/max length comparison, added
+  // unsigned to shut the compiler up
+  unsigned int min = 13;
+  unsigned int max = 16;
 
   while (1) {
     cc_str = get_string("Please enter a card number: ");
     if (cc_str != NULL) {
+      unsigned int len = strlen(cc_str);
       if (checkSpecialChars(cc_str)) {
         printf("Error: Contains special characters - ");
       } else {
-        if (strlen(cc_str) >= min && strlen(cc_str) <= max) {
+        if (len >= min && len <= max) {
           break;
         } else {
           printf("Error: Invalid character length - ");
@@ -68,7 +71,7 @@ int main() {
     cc[i] = cc_str[i] - '0';
   }
 
-  printArrayDebug(cc, len);
+  // printArrayDebug(cc, len, "cc");
 
   // Size the length of the array and then determine if odd or even to find
   // accurate halfway point
@@ -79,43 +82,74 @@ int main() {
   } else {
     new_len = (cc_len + 1) / 2;
   }
-  // BUG:
-  // printf("%d\n", new_len);
 
   // Malloc is needed because we dont know the new size till compile time
-  int *half_cc = (int *)malloc(new_len + sizeof(int));
-  int j = 0;
-  for (int i = 0; i < len; i += 2) {
-    half_cc[j] = cc[i];
-    j++;
+  int *first_half_cc = (int *)malloc(new_len + sizeof(int));
+  int *second_half_cc = (int *)malloc(new_len + sizeof(int));
+  for (int i = 0, j = 0; i < len; i += 2, j++) {
+    first_half_cc[j] = cc[i];
   }
+  for (int i = 1, j = 0; i < len; i += 2, j++) {
+    second_half_cc[j] = cc[i];
+  }
+  // printArrayDebug(first_half_cc, new_len, "first half cc");
+  // printArrayDebug(second_half_cc, new_len, "second half cc");
 
-  // Reverse and multiply the digits by 2
-  reverseArray(half_cc, new_len);
-  printArrayDebug(half_cc, new_len);
+  // Reverse both halfs
+  reverseArray(first_half_cc, new_len);
+  reverseArray(second_half_cc, new_len);
 
+  // multiply fist half
   int digit_arr[new_len];
   for (int i = 0; i < new_len; i++) {
-    digit_arr[i] = (half_cc[i] * 2);
+    digit_arr[i] = (first_half_cc[i] * 2);
   }
-  printArrayDebug(digit_arr, new_len);
+  // printArrayDebug(digit_arr, new_len, "digit arr");
 
-  // NOTE: WE ARE HERE
-  // Sum the digits (Expected sum of 13 with test number)
+  // Sum the digits not the values (Expected sum of 13 with test number)
+  // NOTE: We should extract this logic into it's own function DO THIS TO CLEAN
+  // UP THIS AND THE FOLLOWING!!!!
+  int first_sum = 0;
+  int tmp, digit;
+  for (int i = 0; i < new_len; i++) {
+    tmp = digit_arr[i];
+    while (tmp != 0) {
+      digit = tmp % 10;
+      first_sum += digit;
+      tmp /= 10;
+    }
+  }
+  // printf("fist sum = %d\n", first_sum);
 
-  // NOTE: Remember to free up the malloc
-  free(half_cc);
+  // NOTE: Part 2 - Add the sum of the first half to the sum of the second half
+  // of the digits not multiplied by 2
+  int second_sum = 0;
+  int nu_tmp, nu_digit;
+  for (int i = 0; i < new_len; i++) {
+    nu_tmp = second_half_cc[i];
+    while (nu_tmp != 0) {
+      nu_digit = nu_tmp % 10;
+      second_sum += nu_digit;
+      nu_tmp /= 10;
+    }
+  }
+  // printf("second sum = %d\n", second_sum);
+
+  int total_sum = first_sum + second_sum;
+  // printf("total sum = %d\n", total_sum);
+
+  // NOTE: Part 3 - If the total's modulo 10 is congruent to 0 the number is
+  // valid
+  // TODO: Final - Add logic to print the card type instead of VALID
+  if (total_sum % 10 == 0) {
+    printf("VALID");
+  } else {
+    printf("INVALID");
+  }
+
+  // NOTE: Remember to free up the heap space used by malloc
+  free(first_half_cc);
   return 0;
 }
 
 // Test number 4003600000000014
-
-// int sum = 0;
-// int tmp;
-// for (int i = 0; i < new_len; i++) {
-//   tmp = (half_cc[i] * 2);
-//   sum += tmp;
-//   tmp = 0;
-// }
-
-// printf("%d", sum);
