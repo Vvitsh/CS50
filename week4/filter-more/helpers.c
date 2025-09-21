34587d636f81db6a4e7dbeb74c4b03a40ed65dcb
Emma Junger <ejjunger@proton.me>
2025-09-20 23:33:03 -0400

    task 2 blur image filter done

--- week4/filter-more/helpers.c
+++ week4/filter-more/helpers.c
@@ -1,4 +1,5 @@
 #include "helpers.h"
+#include <math.h>
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width]) {
@@ -8,14 +9,16 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width]) {
 
       double sum =
           image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed;
+
       double avg = sum / 3.0;
 
-      RGBTRIPLE nuimage = {nuimage.rgbtBlue = avg, nuimage.rgbtGreen = avg,
-                           nuimage.rgbtRed = avg};
+      RGBTRIPLE grayscale_bm = {
+          .rgbtBlue = avg, .rgbtGreen = avg, .rgbtRed = avg};
 
-      image[i][j] = nuimage;
+      image[i][j] = grayscale_bm;
     }
   }
+
   return;
 }
 
@@ -43,7 +46,43 @@ void reflect(int height, int width, RGBTRIPLE image[height][width]) {
 }
 
 // Blur image
-void blur(int height, int width, RGBTRIPLE image[height][width]) { return; }
+void blur(int height, int width, RGBTRIPLE image[height][width]) {
+
+  int offsets[8][2] = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1},
+                       {0, 1},   {1, -1}, {1, 0},  {1, 1}};
+  int set_len = sizeof(offsets) / sizeof(offsets[0]);
+
+  for (int i = 0; i < height; i++) {
+    for (int j = 0; j < width; j++) {
+
+      int blue_channel = 0;
+      int green_channel = 0;
+      int red_channel = 0;
+      int count = 0;
+
+      for (int k = 0; k < set_len; k++) {
+
+        int x = i + offsets[k][0];
+        int y = j + offsets[k][1];
+
+        if (x >= 0 && x < height && y >= 0 && y < width) {
+
+          blue_channel += image[x][y].rgbtBlue;
+          green_channel += image[x][y].rgbtGreen;
+          red_channel += image[x][y].rgbtRed;
+
+          count++;
+        }
+      }
+
+      image[i][j].rgbtBlue = roundf((float)blue_channel / count);
+      image[i][j].rgbtGreen = roundf((float)green_channel / count);
+      image[i][j].rgbtRed = roundf((float)red_channel / count);
+    }
+  }
+
+  return;
+}
 
 // Detect edges
 void edges(int height, int width, RGBTRIPLE image[height][width]) { return; }
