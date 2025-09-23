84f753947cbcd3d73aaf33897c872ce454102c00
Emma Junger <ejjunger@proton.me>
2025-09-23 01:07:28 -0400

    trying to work out the chunks

--- week4/recover/recover.c
+++ week4/recover/recover.c
@@ -3,6 +3,11 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define BLOCK_SIZE 512
+
+unsigned char *find_header(unsigned char *buffer, size_t buffer_len,
+                           unsigned char *header, size_t header_len);
+
 int main(int argc, char *argv[]) {
 
   if (argc < 2 || argc > 2) {
@@ -10,31 +15,50 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
-  char *input_file = argv[1];
+  char *input = argv[1];
 
-  FILE *fp = fopen(input_file, "r");
-  if (fp == NULL) {
-    printf("Could not open %s.\n", input_file);
+  FILE *file = fopen(input, "rb");
+  if (file == NULL) {
+    printf("Error: Could not open %s\n", input);
     return 4;
   }
 
-  char jpg_header[] = {0xFF, 0xd8, 0xFF}; // 0xe0 handle 4th byte?
+  unsigned char jpg_header[] = {0xFF, 0xd8, 0xFF}; // 0xe0 handle 4th byte?
   size_t header_len = sizeof(jpg_header);
 
-  int bytes_read;
-  char buffer[512];
-  size_t buffer_len = sizeof(buffer);
-  long offset = 0;
-
-  while ((bytes_read = fread(buffer, 1, buffer_len, fp)) > 0) {
-    for (int i = 0; i < bytes_read - header_len; ++i) {
-      if (memcmp(&buffer[i], jpg_header, header_len) == 0) {
-      }
-    }
-    offset += bytes_read;
+  unsigned char *buffer = malloc(BLOCK_SIZE + header_len - 1);
+  if (!buffer) {
+    perror("Error: malloc failed\n");
+    fclose(file);
+    return 1;
   }
 
-  fclose(fp);
+  size_t block_overlap = 0;
+  long offset = 0;
+  size_t bytes;
+
+  while ((bytes = fread(buffer + block_overlap, 1, BLOCK_SIZE, file)) > 0) {
+    size_t total = bytes + block_overlap;
+
+    unsigned char *match = find_header(buffer, total, jpg_header, header_len);
+  }
+
+  free(buffer);
+  fclose(file);
 
   return 0;
+}
+
+unsigned char *find_header(unsigned char *buffer, size_t buffer_len,
+                           unsigned char *header, size_t header_len) {
+  if (header_len == 0 || buffer_len < header_len)
+    return NULL;
+
+  for (size_t i = 0; i <= buffer_len - header_len; i++) {
+    if (memcmp(buffer + i, header, header_len) == 0) {
+      return buffer + i;
+    }
+  }
+
+  return NULL;
 }
