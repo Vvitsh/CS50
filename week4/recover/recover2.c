76f62bfa35e9f42b7754e6ec58a8495532b64a71
Emma Junger <ejjunger@proton.me>
2025-09-26 17:49:59 -0400

    Cleaned up solution and added comments and error checks

--- week4/recover/recover2.c
+++ week4/recover/recover2.c
@@ -28,31 +28,39 @@ int main(int argc, char *argv[]) {
   // While there's still data left to read from the memory card
   while (fread(buffer, 1, 512, raw_file) == 512) {
 
-    // Create JPEGs from the data
+    // If the header is found in a block's first four bytes
     if (buffer[0] == 0xFF && buffer[1] == 0xD8 && buffer[2] == 0xFF &&
         (buffer[3] & 0xF0) == 0xE0) {
 
+      // Close the output file if it is in use
       if (out) {
         fclose(out);
       }
 
+      // Create a filename and open our output file
       char filename[64];
       sprintf(filename, "%03d.jpg", ++jpg_count);
-
       out = fopen(filename, "w");
-      fwrite(buffer, 1, 512, out);
 
-    } else {
-
-      if (out) {
-        fwrite(buffer, 1, 512, out);
+      // Handle the error if fopen fails
+      if (!out) {
+        printf("Error: Could not create %s\n", filename);
+        fclose(raw_file);
+        return 2;
       }
+    }
+
+    // If an output file is open, write the current bytes to file
+    if (out) {
+      fwrite(buffer, 1, 512, out);
     }
   }
 
+  // Close out output file
   if (out)
     fclose(out);
 
+  // Close out raw file
   fclose(raw_file);
 
   return 0;
